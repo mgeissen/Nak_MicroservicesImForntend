06b4aa708ac979301b50fd5934768ae70686553e
mgeissen <matt.geis97@gmail.com>
2020-03-07 14:02:00 +0100

    add initial spring application

---
+++ src/test/java/de/nordakademie/microservice/MicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package de.nordakademie.microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
